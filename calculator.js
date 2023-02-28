53d58820d6cdc830cf33683eee0f4e9438402f0e
MBanerjea <mayukh.flip22@gmail.com>
2023-02-28 23:51:52 +0530

    sets up the add and subtract functions.

---
+++ calculator.js
@@ -0,0 +1,15 @@
+// Basic functions
+
+// Add
+function add(numbers) {
+  let result = numbers.reduce((sum, current) => sum + current, 0);
+  return result;
+}
+
+// Subtract
+function subtract(numbers) {
+  let result = number.reduce((remainder, current) => remainder - current)
+  return result;
+}
+
+// 
