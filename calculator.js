6b731c7b0bef64b41fe5f56ee2e56a03196b3cf2
MBanerjea <mayukh.flip22@gmail.com>
2023-03-06 17:18:49 +0530

    complete calculator except extra credits

--- calculator.js
+++ calculator.js
@@ -2,6 +2,7 @@
 
 // Operate
 function operate(operator, a, b) {
+
   switch (operator) {
     case '+':
       return add(a, b);
@@ -9,7 +10,7 @@ function operate(operator, a, b) {
     case '-':
       return subtract(a, b);
 
-    case '*':
+    case 'x':
       return multiply(a, b);
 
     case '/':
@@ -21,17 +22,17 @@ function operate(operator, a, b) {
 }
 
 // Add
-function add(numbers) {
+function add(a, b) {
   return a + b;
 }
 
 // Subtract
-function subtract(numbers) {
+function subtract(a, b) {
   return a - b;
 }
 
 // Multiply
-function multiply(numbers) {
+function multiply(a, b) {
   return a * b;
 }
 
@@ -39,4 +40,75 @@ function multiply(numbers) {
 function divide(a, b) {
   return a / b;
 }
+
+
+// Display population
+const clear = document.querySelector('#clear');
+const equals = document.querySelector('#equals');
+const operators = document.querySelectorAll('.operators');
+const numbers = document.querySelectorAll(".numbers")
+const displayScreen = document.querySelector(".display");
+
+let displayValue = "";
+let currentValue = "";
+let previousValue = "";
+let operator = "";
+
+
+// numbers
+numbers.forEach(number => {
+  number.addEventListener('click', (e) => {
+    handleNumbers(e.target.textContent);
+    displayScreen.textContent = currentValue;
+  })
+})
+
+function handleNumbers(num) {
+  if (currentValue.toString().length <= 5) {
+    currentValue += num;
+  }
+}
+
+// operators
+operators.forEach(operatorBtn => {
+  operatorBtn.addEventListener('click', (e) => {
+    if (operator != "") {
+      currentValue = operate(operator, Number(previousValue), Number(currentValue));
+    }
+    handleOperators(e.target.textContent);
+    displayScreen.textContent = currentValue;
+  })
+})
+
+function handleOperators(op) {
+  if (currentValue != "") {
+    previousValue = currentValue;
+  }
+  currentValue = "";
+  operator = op;
+}
+
+// calculate
+
+equals.addEventListener('click', () => {
+  if (currentValue != "" && previousValue != "") {
+    currentValue = operate(operator, Number(previousValue), Number(currentValue))
+    displayScreen.textContent = currentValue;
+    operator = "";
+    previousValue = currentValue;
+    currentValue = "";
+  }
+
+});
+
+
+// clearance
+
+clear.addEventListener('click', () => {
+  displayScreen.textContent = "0";
+  currentValue = "";
+  previousValue = "";
+  operator = "";
+})
+
 
